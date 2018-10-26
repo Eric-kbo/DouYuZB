8c8239d11a1576e127c6664ebd2cdd6c421f5e8b
Zhou <zhouxingbang@eric-mbp.local>
2018-10-27 00:20:43 +0800

    文件统一上传

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by Eric on 2018/10/22.
+//  Copyright © 2018 eric. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let vc=UIViewController();
+        vc.view.backgroundColor=UIColor.blue;
+        
+        addChild(vc)
+        
+    }
+    
+
+}
